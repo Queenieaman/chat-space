81f04e08462c784f5197e7b571ce532b295700b2
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-05-13 20:52:44 +0900

    Update users_controller.rb
    set action: edit.  update. user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit  # editアクションのviewを表示
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
+
+# The former(require) is used to mark parameters as required. The latter(permit) is used to set the parameter as permitted and limit which attributes should be allowed for mass updating.
