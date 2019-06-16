098ffcf5748a714ccf34b45a9d608bc5ddc50fa4
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-06-16 15:36:32 +0900

    Modify carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'yuima-upload-test'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yuima-upload-test'
 end
