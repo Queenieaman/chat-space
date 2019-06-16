a8c46aca3064d0cfa647b39cf39d300eb96f155c
Queenie <49942420+Queenieaman@users.noreply.github.com>
2019-06-17 08:19:13 +0900

    Modify application.rb
    set timezone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    #デフォルトは協定世界時(UTC)らしいのでそれをJSTに直す必要がある
+    # 表示時のタイムゾーンをJSTに設定
+    config.time_zone = 'Tokyo'
+    # DB保存時のタイムゾーンをJSTに設定
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
